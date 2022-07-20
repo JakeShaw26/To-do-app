dd43ad2ea56bd87527a98557a8a60dd856b20b14
JakeShaw26 <jacob.shaw@and.digital>
2022-07-20 11:58:10 +0100

    Main interface for To Do List

---
+++ my-to-do/src/index.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import TodoContainer from "./Components/TodoContainer/TodoContainer";
+ReactDOM.render(<TodoContainer />, document.getElementById("root"));
