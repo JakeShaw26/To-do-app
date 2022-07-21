0dec47d357328faeaf0772ab45caaf0d835c71af
JakeShaw26 <jacob.shaw@and.digital>
2022-07-21 14:13:19 +0100

    push to gh pages

--- my-to-do/src/index.jsx
+++ my-to-do/src/index.jsx
@@ -5,7 +5,7 @@ import ".././src/Styles/index.css";
 // import "./classBased/App.css";
 import TodoContainer from "./functionBased/Components/TodoContainer/TodoContainer";
 import "./functionBased/App.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
